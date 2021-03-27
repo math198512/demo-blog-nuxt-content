fd614c16c294780087c9c6b21bc60c8593909d4b
math198512 <57964579+math198512@users.noreply.github.com>
2021-03-27 22:40:31 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,7 @@
 <style>
 html {
   @import url(http://fonts.googleapis.com/earlyaccess/amstelvaralpha.css);
-
+  font-family: 'Amstelvar Alpha', serif;
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
   font-size: 16px;
