c541eb90f1a2960c7923f20102f2580395b5cf15
math198512 <57964579+math198512@users.noreply.github.com>
2021-03-27 22:46:55 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,8 +6,9 @@
 
 <style>
 html {
-  @import url(http://fonts.googleapis.com/earlyaccess/amstelvaralpha.css);
-  font-family: 'Amstelvar Alpha', serif;
+  /*@import url(http://fonts.googleapis.com/earlyaccess/amstelvaralpha.css);*/
+  @import url(//db.onlinewebfonts.com/c/280e0867d189623928fcc0d7cfdaaa47?family=Arabic+Typesetting);
+  font-family: 'Arabic+Typesetting', serif;
   /*font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;*/
   font-size: 16px;
